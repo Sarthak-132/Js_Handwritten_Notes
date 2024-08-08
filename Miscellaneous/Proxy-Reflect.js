88c765458020cfb321e8749bc8729dee930a9ac6
unknown <sarthakdhoke132@gmail.com>
2024-08-08 20:36:31 +0530

    some content added

---
+++ Miscellaneous/Proxy-Reflect.js
@@ -0,0 +1,3 @@
+// A Proxy object wraps another object and intercepts operations, like reading/writing properties and others, optionally handling them on its own, or transparently allowing the object to handle them.
+
+// Proxies are used in many libraries and some browser frameworks. We’ll see many practical applications in this article.
