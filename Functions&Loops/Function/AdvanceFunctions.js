ddc55853c6a42e8ce76adcd457c6812a297e8af4
unknown <sarthakdhoke132@gmail.com>
2024-10-14 06:57:52 +0530

    lexical Environment

--- Functions&Loops/Function/AdvanceFunctions.js
+++ Functions&Loops/Function/AdvanceFunctions.js
@@ -99,7 +99,114 @@ function hello(){
 
 
 // function inside function
+const firstfunc = () =>{
+    const secondFunc = () =>{
+        console.log("How are you......?")
+    }
+
+    const addThirdfunc = (val1 , val2) =>{
+        return val1 + val2;
+    }
+    console.log("Hey");
+
+
+    secondFunc();
+    console.log(addThirdfunc(1,2));
+}
+
+firstfunc();         // Hey   .... it happens when only firstfunc() is called and no other function is called
+ 
+// outcome - 
+// Hey 
+// How are you......?
+// 3
 
 
 
 
+// Lexical scope -  lexical scope is the concept of determining the scope of a variable based on its declaration. 
+//                  This means that the scope of a variable is determined by the block of code in which it is declared, 
+//                  and not by the block of code in which it is used.
+ 
+// The oneFun() function does not return a value, so it returns undefined by default. console.log(oneFun()); 
+// logs the return value of oneFun(), which is undefined, to the console.
+// value
+// undefined
+// function oneFun(){
+//     const myVar = "value";
+//     function twoFun(){}
+//     const newFun = function (){}
+//     const arrowFun = () => {}
+//     console.log(myVar);
+// }
+// console.log(oneFun());
+
+
+
+// same thing will come as output     // value // inside Function twoFun // undefined
+
+// function oneFun(){
+//     const myVar = "value";
+//     function twoFun(){
+//         console.log("inside Function twoFun");
+//     }
+//     const newFun = function (){}
+//     const arrowFun = () => {}
+//     console.log(myVar);
+//     twoFun();
+// }
+// console.log(oneFun());
+
+
+
+// same thing will come as output     // value // inside Function twoFun 345 // undefined
+// HERE myVar variables's value is present in function twoFun() and as well in oneFun() so it will print 345 val.. of twoFun()
+// function oneFun(){
+//     const myVar = "value";
+//     function twoFun(){
+//         const myVar = "345"
+//         console.log("inside Function twoFun", myVar);
+//     }
+//     const newFun = function (){}
+//     const arrowFun = () => {}
+//     console.log(myVar);
+//     twoFun();
+// }
+// console.log(oneFun());
+
+
+// same thing will come as output     // value // inside Function twoFun value // undefined
+// HERE myVar variables's value is not present in function twoFun() but in oneFun() it is present so it will print value
+// if it is outside the function still it will print value
+
+// const myVar = "value";
+// function oneFun(){
+//     const myVar = "value";
+//     function twoFun(){
+//         // const myVar = "345"
+//         console.log("inside Function twoFun", myVar);
+//     }
+//     const newFun = function (){}
+//     const arrowFun = () => {}
+//     console.log(myVar);
+//     twoFun();
+// }
+// console.log(oneFun());
+
+
+
+// 
+const myVar = "value";
+function oneFun(){
+
+    function twoFun(){
+        const arrowFun = () => {
+            console.log ("inside Function twoFun", myVar);
+        }
+        arrowFun();     //twoFun() is called - ERROR (maximum call stack size exceeded)
+    }
+    
+    console.log(myVar);
+    twoFun();
+}
+console.log(oneFun());
