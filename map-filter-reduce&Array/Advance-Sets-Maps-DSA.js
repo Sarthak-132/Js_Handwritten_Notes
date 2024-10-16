b3631e315cd1d669fad21063278077f72f6ef927
unknown <sarthakdhoke132@gmail.com>
2024-10-16 22:03:58 +0530

    Map in js and object chaining and cloning

--- map-filter-reduce&Array/Advance-Sets-Maps-DSA.js
+++ map-filter-reduce&Array/Advance-Sets-Maps-DSA.js
@@ -89,4 +89,98 @@ console.log(length);
 // like - array, number, String 
 
 // Key value pair like object
-const Persons = 
+const Persons = new Map();
+Persons.set("name", "Harrish");
+Persons.set("age", 21);
+Persons.set(1, "one");
+Persons.set([1,2,3,4], "Array");
+console.log(Persons);
+// console.log(Persons.name);      // this will give undefined in map
+// console.log(Persons['name']);   // this will give undefined in map
+
+// Methods 
+console.log(Persons.get("name"));
+console.log(Persons.keys());
+
+for(let key of Persons.keys()){
+    console.log(key, typeof key);
+}
+
+for(let key of Persons){
+    console.log(typeof key);
+    console.log(Array.isArray(key));
+}
+
+for(let [key, value] of Persons){         // need to destructure like that
+    console.log(typeof value);
+}
+
+
+
+// in map array in array
+const arrayIn = new Map([['we','centre'],['you','desktop'],['me','laptop']]);
+console.log(arrayIn);
+
+
+// suppose we have object and we want to add additional things in that object using Map 
+
+const obj = {
+    id : 1,
+    fname : "harsha",
+}
+const obj2 = {
+    id : 1,
+    fname : "harsha",
+}
+
+const userInfo = new Map();
+userInfo.set(obj, {age: 8, gender:"male"});
+userInfo.set(obj2, {age: 12, gender:"female"});
+console.log(userInfo);
+console.log(obj.id);
+console.log(userInfo.get(obj));
+console.log(userInfo.get(obj).age); 
+console.log(userInfo.get(obj2).age); 
+
+
+
+// clone using object
+
+const object = {
+    key1 : "val1",
+    key2 : "val2",
+}
+const object2 = object;
+object.key3 = "val3";
+
+console.log(object);       // { key1: 'val1', key2: 'val2' }
+console.log(object2);      // { key1: 'val1', key2: 'val2' }
+
+
+// otherswise do this if want new object using cloning
+// const object3 = Object.assign({}, object);         or
+const object3 = {...object};
+object.key4 = "val4";
+console.log(object);       // { key1: 'val1', key2: 'val2' }
+console.log(object3); 
+
+
+
+
+
+// optional chaining
+
+const device = {
+    fname : "harsha",
+    address : {house : "1234"}
+}
+console.log(device.fname);
+console.log(device.address.house);
+
+
+// we dont have some key and we access them like
+// console.log(device.age.number);             // this will give an error can not read property "number" of undefined
+console.log(device.age?.number);             // undefined print if use ? (if user exist then outout otherwise undefined)
+console.log(device?.age?.number);             // undefined print if use ? (if user exist then outout otherwise undefined)
+console.log(device.age);                       // undefined
+
