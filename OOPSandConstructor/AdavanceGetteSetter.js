35a470e55fbb109f4d19010fbbc05b3c4745e014
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-10-24 22:16:23 +0530

    getterSetter method

--- OOPSandConstructor/AdavanceGetteSetter.js
+++ OOPSandConstructor/AdavanceGetteSetter.js
@@ -1,27 +1,38 @@
-class Animal {
-    constructor(name) {
-        this._name = name
-    }
-    fly() {
-        alert("I'm flying")
-    }
-    get name(){
-        return this._name
-    }
+// class Animal {
+//     constructor(name) {
+//         this._name = name
+//     }
+//     fly() {
+//         alert("I'm flying")
+//     }
+//     get name(){
+//         return this._name
+//     }
 
-    set name(newName){
-        this._name = newName
+//     set name(newName){
+//         this._name = newName
+//     }
+// }
+
+// let a = new Animal ('Bruno');
+// a.fly()
+// console.log(a.name)
+
+
+
+
+
+class Person {
+    constructor(firstName , lastName , age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
     }
 }
 
-let a = new Animal ('Bruno');
-a.fly()
-console.log(a.name)
+const person_1 = new Person("harshit" , "sharma" , 6);
+console.log(person_1);
 
-
-
-
-
-class person {
-    
-}
