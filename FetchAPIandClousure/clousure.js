33af459624de3f0b035da4cc729dfe779695f78f
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-10-28 20:43:59 +0530

    how js works and closure concept in detail

--- FetchAPIandClousure/clousure.js
+++ FetchAPIandClousure/clousure.js
@@ -15,3 +15,85 @@ first_function();
 
 // if variables are declared in it's lexical environment then it is call otherwise it will gives access denied.
 // like if mozilla out of it's function then it will not call
+
+
+
+
+// closures in JS 
+
+// Example - 1
+function printFullName (fName , lName){
+    function printName(){
+        console.log(fName, lName);
+    }
+    return printName;
+}
+
+const ans = printFullName("harshit" , "sharma");
+console.log(ans());
+
+
+
+// Example - 2
+function hello(exe){
+    const a = "varA"
+    const b = "varB"
+    return function (){
+        console.log(a, b, exe);
+    }
+}
+
+const res = hello("arg");
+res();
+
+
+
+
+//  Example - 3
+// function myFunction(power){
+//     return function(number){
+//         return number ** power;
+//     }
+// } 
+
+// or 
+
+// const myFunction = (power) => {
+//     return (number) => number ** power; 
+// } 
+
+// or
+
+const myFunction = power => number => number ** power; 
+
+const one = myFunction(2);
+const two = one(2);
+console.log(two);
+
+const three = myFunction(3);
+const four = three(2);
+console.log(four);
+
+
+
+
+
+// example - 3
+
+const call = () => {
+    let count = 0;
+    return fun = () => {
+        if(count < 1){
+            console.log("Hi , you called me!");
+            count++;
+        }
+        else{
+            console.log("already run");
+        }
+    }
+}
+
+const funCall = call();
+funCall();
+funCall();
+funCall();
--- JS_WORKS/working.js
+++ JS_WORKS/working.js
@@ -31,19 +31,67 @@
 // i.   creation phase (CP)
 // ii.  code execution phase (CE)
 
+// after these things one more thing present in js that is "callstack" - it is take care of which line execute 
+// which execution context is working and which function is call. it tracks how many lines of code is run until now
+
+// when function get arguments or call then another one execution context created then it will add in callstack called
+// function execution context.
+
+
 // during the creation phase of global execution context varibale get alloted memory in global memory using var variable 
 // meanwhile in the function keyword used function created save in memory directly so it will print function instead of 
-// undefined like variable in global execution context. 
- 
+// undefined like variable in global execution context. one more thing that if function expression (funciton name ) gives
+// by var variable then it will declare it's space as undefined.
+
+
+// in let and const case variable will be uninitialized instead of undefined. it means variable is created and got memory
+// but not initialized. on run the code it shows uncaught ReferenceError - cannot access 'fname' before initialization.   
+// let and const are hoisted but it is uninitialized.
 
 
 // Hoisting - before execution this things storing in memory
 
 
+// TDZ - temporal dead zone - A block’s temporal dead zone starts at the beginning of the block’s local scope. 
+// It ends when the computer fully initializes your variable with a value.
+// Below are examples illustrating where a block’s temporal dead zone starts and ends.
 
 
-console.log(this);
-console.log(window);
+// {
+    // bestFood's TDZ starts here (at the beginning of this block's local scope)
+    // bestFood's TDZ continues here
+    // bestFood's TDZ continues here
+    // bestFood's TDZ continues here
+    // console.log(bestFood); // returns ReferenceError because bestFood's TDZ continues here
+    // bestFood's TDZ continues here
+    // bestFood's TDZ continues here
+    // let bestFood = "Vegetable Fried Rice"; // bestFood's TDZ ends here
+    // bestFood's TDZ does not exist here
+    // bestFood's TDZ does not exist here
+    // bestFood's TDZ does not exist here
+//   }
+ 
+// bestFood's TDZ continues here
+// let bestFood; // bestFood's TDZ ends here
+// console.log(bestFood); // returns undefined because bestFood's TDZ does not exist here
+// bestFood = "Vegetable Fried Rice"; // bestFood's TDZ does not exist here
+// console.log(bestFood); // returns "Vegetable Fried Rice" because bestFood's TDZ does not exist here
+
+
+var fname;
+console.log(fname);                     // undefined
+
+let fullName ;
+console.log(fullName);                  // undefined  
+
+// const fame;
+// console.log(fame);                   // uncaught syntaxerror = missing initializer in const declaration 
+
+
+
+
+// console.log(this);
+// console.log(window);
 console.log(first);                     // undefined
 var first = "harsha";                   
 console.log(first);                     // harsha
@@ -52,3 +100,4 @@ console.log(first);                     // harsha
 
 
 
+// function execution context - 
