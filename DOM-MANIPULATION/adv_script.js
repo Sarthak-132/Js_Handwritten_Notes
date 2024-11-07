986054d107b9ecbd42134dc670ed3b7d12bea371
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-11-07 22:51:05 +0530

    targetting elements method learned

--- DOM-MANIPULATION/adv_script.js
+++ DOM-MANIPULATION/adv_script.js
@@ -1,6 +1,5 @@
 // change text 
 // textContent and innerText    
-
 const heading = document.getElementById("heading");
 console.log(heading.textContent);
 heading.textContent = "Manage";
@@ -8,7 +7,6 @@ console.log(heading.textContent);
 
 
 // to target particular element in html and style them
-
 const logo = document.querySelector("header nav h1");
 console.log(logo);
 logo.style.backgroundColor = "red";
@@ -27,3 +25,27 @@ console.log(inputElem.getAttribute("type"));
 
 // get multiple elements using getElementsByClassName
 // get multiple elements using querySelectorAll
+
+// getElementsByClassNam - can not iterate html collection through forEach method instead use (for , for of)
+const navItems = document.getElementsByClassName("nav-items");
+console.log(navItems);       //these are array like objects -> html collection
+// so limited methods are available not all the array methods can be used
+// array like objects - indexing , length property , 
+console.log(navItems[1]);
+console.log(typeof navItems); // object
+
+// querySelectorAll
+const navItems2 = document.querySelectorAll(".nav-items");
+console.log(navItems2);         //Nodelist 
+console.log(navItems2[2]); 
+console.log(typeof navItems2);  // object-> 
+
+
+// loop using
+// getElementsByTagName
+const navItems3 = document.getElementsByTagName("a");
+for(let i = 0 ; i < navItems3.length ; i++){
+    const value = navItems3[i]; 
+    value.style.backgroundColor = "#fff";
+    value.style.color = "#000";
+}
